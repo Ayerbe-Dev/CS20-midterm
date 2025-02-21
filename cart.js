349e8ab119e6192204592b32eaa3e05e350f05c7
Henry Nissen <63805100+Ayerbe-Dev@users.noreply.github.com>
2025-02-21 13:31:06 -0500

    Cart-related functionality
    Wrote a js file containing functions that will operate on our cart (should be stored across multiple pages), renamed shopping_cart.html to just cart.html

---
+++ cart.js
@@ -0,0 +1,52 @@
+//addToCart: adds [itemNum] copies of [itemName] to our cart
+function addToCart(itemName, itemNum) {
+    const cartNameString = localStorage.getItem('cartNames');
+    const cartNames = cartNameString ? JSON.parse(cartNameString) : [];
+    const cartNumString = localStorage.getItem('cartNums');
+    const cartNums = cartNumString ? JSON.parse(cartNumString) : [];
+    cartNames.push(itemName);
+    cartNums.push(itemNum);
+    localStorage.setItem('cartNames', JSON.stringify(cartNames));
+    localStorage.setItem('cartNums', JSON.stringify(cartNums));
+}
+
+//incCartNum: increases the number of copies of [itemName] in our cart by 1
+function incCartNum(itemName) {
+    const cartNameString = localStorage.getItem('cartNames');
+    const cartNames = cartNameString ? JSON.parse(cartNameString) : [];
+    const cartNumString = localStorage.getItem('cartNums');
+    const cartNums = cartNumString ? JSON.parse(cartNumString) : [];
+    var idx = cartNames.indexOf(itemName);
+    cartNums[idx]++;
+    localStorage.setItem('cartNums', JSON.stringify(cartNums));
+}
+
+//decCartNum: decreases the number of copies of [itemName] in our cart by 1, 
+//            removing [itemName] entirely if this results in 0 copies of it
+function decCartNum(itemName) {
+    const cartNameString = localStorage.getItem('cartNames');
+    const cartNames = cartNameString ? JSON.parse(cartNameString) : [];
+    const cartNumString = localStorage.getItem('cartNums');
+    const cartNums = cartNumString ? JSON.parse(cartNumString) : [];
+    var idx = cartNames.indexOf(itemName);
+    cartNums[idx]--;
+    if (cartNums[idx] == 0) {
+        localStorage.setItem('cartNums', JSON.stringify(cartNums));
+    }
+    else {
+        removeFromCart(itemName);
+    }
+}
+
+//removeFromCart: removes all copies of [itemName] from our cart
+function removeFromCart(itemName) {
+    const cartNameString = localStorage.getItem('cartNames');
+    const cartNames = cartNameString ? JSON.parse(cartNameString) : [];
+    const cartNumString = localStorage.getItem('cartNums');
+    const cartNums = cartNumString ? JSON.parse(cartNumString) : [];
+    var idx = cartNames.indexOf(itemName);
+    cartNames.splice(idx, 1);
+    cartNums.splice(idx, 1);
+    localStorage.setItem('cartNames', JSON.stringify(cartNames));
+    localStorage.setItem('cartNums', JSON.stringify(cartNums));
+}
